3e93aba7c21c12b8915d8b82f23b5012392dbad8
Andre Regino <reginopanai@gmail.com>
2024-05-04 19:44:06 -0600

    installed mongoose

--- server.js
+++ server.js
@@ -6,9 +6,61 @@
 /* ******************************************
  * Require Statements
  *******************************************/
-const express = require('express')
-const app = express()
-const baseController = require('./controllers/baseController')
+const express = require('express');
+const app = express();
+const baseController = require('./controllers/baseController');
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+const env = require("dotenv").config()
+
+
+/* ******************************************
+* Middleware
+*******************************************/
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+  });
+
+/* ***********************
+ * Functions
+ *************************/
+
+async function main() {
+ 
+    const uri = process.env.DATABASE_URL;
+    //const client = new MongoClient(uri,{ useUnifiedTopology: true });
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const client = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+    db.once('open', () => {
+        console.log('Connected to MongoDB');
+      });
+    // try {
+    //     const response = await client.connect();
+    //     console.log(response)
+    //     await listDatabases(client);
+    // } catch (e) {
+    //     console.error(e);
+    // } finally {
+    //     await client.close();
+    // }
+}
+
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
+ 
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
+
+main().catch(console.error);
+
+
 
 /* ***********************
  * Routes
@@ -29,6 +81,7 @@ app.get('/self', (req, res) =>{
  * Server Listener
  *************************/
 const port = 3000
+
 
 app.listen(process.env.PORT || port, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT || port}`)
