cac90efebb7e1160c86af38b29276156838bf734
Andre Regino <reginopanai@gmail.com>
2024-05-18 17:11:38 -0600

    added cors to server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const router = require('./routes/router.js');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
+const cors = require('cors');
 
 
 /* ******************************************
@@ -22,11 +23,12 @@ const swaggerDocument = require('./swagger-output.json');
 //   });
 
 // next();
+app.use(cors());
+
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(bodyParser.json());
-app
- .use((req, res, next) => {
+
+app.use(bodyParser.json()).use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -36,12 +38,6 @@ app
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
  })
-/* ***********************
- * Router
- *************************/
-
-
-// Send everything to the router handler
 app.use('/', router);
 
 
